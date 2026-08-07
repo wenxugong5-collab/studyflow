d9ae1281685871ee8b43df5059ca944ddea68e49
Developer <dev@studyflow.app>
2026-08-07 10:05:28 +0800

    修正supabase环境变量名称

--- src/lib/supabaseData.ts
+++ src/lib/supabaseData.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://fdmoamczmyihfsczkrfi.supabase.co"
-// 此处粘贴你在API Keys复制的 anon public 密钥（一大串字母数字）
-const supabaseAnonKey = "sb_publishable_XrbuQwRrz65LcVfnT9L9pQ_MKVkKOiB"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
